235b65d73493692afdd2d287feebf9210e61a6d6
Балканов Богдан <89509494919@mail.ru>
2023-02-10 16:26:42 +0300

    Сделано

---
+++ Задача_про_2х_друзей/Program.cs
@@ -0,0 +1,25 @@
+﻿// Два друга шли на встречу друг к другу.
+int count=0;
+int distance=10000;
+int F1speed =1;
+int F2speed =2;
+int Dspeed =5;
+int friend=2;
+int time=0;
+
+while(distance>10)
+{
+    if(friend==1)
+    {
+        time=distance/(F1speed+Dspeed);
+        friend=2;
+    }
+    else
+    {
+        time=distance/(F2speed+Dspeed);
+        friend=1;
+    }
+distance=distance+(F1speed+F2speed)*time;
+count++;
+}
+Console.WriteLine("Количество раз пробежала собака " + count);
