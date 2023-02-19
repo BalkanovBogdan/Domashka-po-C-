d068644eff9b7022cb927a4a9dfc6e3ac5a0fcaf
Балканов Богдан <89509494919@mail.ru>
2023-02-19 12:32:46 +0300

    18.02.23

---
+++ Задача16/Program.cs
@@ -0,0 +1,14 @@
+﻿// Напишите программу, которая принимает на вход два числа и проверяет, 
+// является ли одно число квадратом другого.
+Console.Clear();
+Console.WriteLine("Введите два числа подрят");
+int a=int.Parse(Console.ReadLine());
+int b=int.Parse(Console.ReadLine());
+if(a*a==b || Math.Pow(b,2)==a)
+{
+    Console.WriteLine("является квадратом");
+}
+else
+{
+    Console.WriteLine("не является квадратом");
+}
