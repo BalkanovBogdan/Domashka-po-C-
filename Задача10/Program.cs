9743febd237d808d148b99deea81681364915bca
Балканов Богдан <89509494919@mail.ru>
2023-02-24 00:01:20 +0300

    Сделал но с ошибкой не знаю как правильно

---
+++ Задача10/Program.cs
@@ -0,0 +1,14 @@
+﻿//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+Console.Clear();
+Console.WriteLine("Введите трехзначное число");
+int num=int.Parse(Console.ReadLine());
+int result=0;
+if(99<num && num<1000)
+{
+    result=((num-num%10)%100)/10;
+    Console.WriteLine($"Второе число->{result}.");
+}
+else
+{
+    Console.WriteLine("Введенно не трехзначное число, повторите заново.");
+}
